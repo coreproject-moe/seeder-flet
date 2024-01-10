aecea75b5a5a773bf3d0db90eaf5c9480b2b73a8
Tokito <tokitousan2005@gmail.com>
2024-01-10 08:34:29 +0530

    Fix: change type (#10)

--- lib/components/appbar/__init__.py
+++ lib/components/appbar/__init__.py
@@ -1,7 +1,6 @@
-from typing import Optional
 import flet as ft
 
-def custom_appbar(**extra_properties: Optional[ft.AppBar]) -> ft.AppBar:
+def custom_appbar(**extra_properties: ft.AppBar | None) -> ft.AppBar:
     appbar = ft.AppBar(
         title=ft.Image(src="/icons/logo.png", height=25),
         center_title=True,
